e926b9449e17f1c040d98f06ecfdf436f43a9dd2
Anton Anikin <anton.anikin@htower.ru>
2017-03-10 09:25:56 +0800

    Fix for plugin names displayed in the shortcut editor

--- plugin.cpp
+++ plugin.cpp
@@ -54,10 +54,9 @@ Plugin::Plugin(QObject* parent, const QVariantList&)
     , m_project(nullptr)
     , m_model(new ProblemModel(this))
 {
-    setComponentName(QStringLiteral("kdevverapp"), i18n("Vera++ Analyzer"));
-
     qCDebug(KDEV_VERAPP) << "setting kdevverapp.rc file";
     setXMLFile("kdevverapp.rc");
+    actionCollection()->setComponentDisplayName(i18n("Vera++ Analyzer"));
 
     rules::init();
 
